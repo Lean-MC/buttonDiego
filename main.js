ea9a122c7908e7a4f8599f981a85a1f4d0932198
Lean <leanmaciel91@gmail.com>
2022-10-15 02:19:20 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+//declaramos los botones
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+
+btn1.addEventListener("click", () => {
+    alert("hola me pulsaste");
+});
+
+btn2.addEventListener("click", () => {
+    alert("hola me pulsaste");
+});
+
+btn3.addEventListener("click", () => {
+    alert("hola me pulsaste");
+});
